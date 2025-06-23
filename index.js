dcfd983ca6430e8788211bedab6403812a360032
Jim Gomez <jim.gomez.48@gmail.com>
2025-06-23 00:40:49 -0700

    Implement initial dummy schema and serve in app

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 import express from "express";
+import { createHandler } from "graphql-http/lib/use/express";
+import { ruruHTML } from "ruru/server";
+import schema from "./data/schema";
 
 const PORT = 8080;
 
 const app = express();
 
 app.get("/", (request, response) => {
-  response.send("GraphQL is amazing!");
+  //   response.send("GraphQL is amazing!");
+  response.send(
+    ruruHTML({
+      endpoint: "/graphql",
+    })
+  );
 });
+
+const root = {
+  product: () => {
+    return {
+      id: 124567890,
+      name: "Widget",
+      description: "Beautiful widget to use in the garden",
+      price: 34.99,
+      soldout: false,
+    };
+  },
+};
+
+app.use(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Running server on localhost:${PORT}/graphql`);
